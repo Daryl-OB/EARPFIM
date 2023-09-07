3b646aadbaba300c8a0cc52b2f214c50ae12a3ab
CAJACHAGUA <kevincajachagua@gmail.com>
2023-09-07 18:45:29 -0500

    agregando

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
 const body = document.querySelector("body"),
   sidebar = body.querySelector(".sidebar"),
   toggle = body.querySelector(".toggle");
   toggle.addEventListener("click", () => {
-  sidebar.classList.toggle("close");
+  sidebar.classList.toggle("close");   
 });
 
